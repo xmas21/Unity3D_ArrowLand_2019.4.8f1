28464fdb0213b1358c7c96f23e9dbcae4569442b
HWChia <66512878+xmas21@users.noreply.github.com>
2020-10-07 23:55:01 +0800

    粒子更動

--- YZUproject/Assets/Scripts/EnemyFar.cs
+++ YZUproject/Assets/Scripts/EnemyFar.cs
@@ -1,6 +1,26 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class EnemyFar : Enemy
 {
+    [Header("子彈")]
+    public GameObject bullet;
 
+
+    protected override void Attack()
+    {
+        base.Attack();
+        StartCoroutine(CreateBullet());
+    }
+
+    private IEnumerator CreateBullet()
+    {
+        yield return new WaitForSeconds(data.NearAttackDelay);
+
+        Vector3 pos = transform.position + new Vector3(0, data.NearAttackPos.y, 0);
+
+        pos += transform.forward * data.NearAttackPos.z;
+
+        Instantiate(bullet, pos, transform.rotation);
+    }
 }
