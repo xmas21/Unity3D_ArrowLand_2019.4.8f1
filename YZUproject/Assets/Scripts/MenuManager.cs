86744e87a30e7319cd2907a5e3c5cd00e795c697
HWChia <66512878+xmas21@users.noreply.github.com>
2020-11-28 21:25:54 +0800

    主選單更新 | 裝備系統

--- YZUproject/Assets/Scripts/MenuManager.cs
+++ YZUproject/Assets/Scripts/MenuManager.cs
@@ -26,6 +26,34 @@ public class MenuManager : MonoBehaviour
     [Header("關卡16按鈕")]
     public Button btn16;
 
+    [Header("武器1畫面")]
+    public GameObject weapon1;
+    [Header("武器2畫面")]
+    public GameObject weapon2;
+    [Header("武器3畫面")]
+    public GameObject weapon3;
+    [Header("武器4畫面")]
+    public GameObject weapon4;
+    [Header("武器5畫面")]
+    public GameObject weapon5;
+    [Header("武器6畫面")]
+    public GameObject weapon6;
+    [Header("沒錢買武器畫面")]
+    public GameObject Nomoney;
+
+
+    [Header("武器2按鈕")]
+    public Button btnwpn2;
+    [Header("武器3按鈕")]
+    public Button btnwpn3;
+    [Header("武器4按鈕")]
+    public Button btnwpn4;
+    [Header("武器5按鈕")]
+    public Button btnwpn5;
+    [Header("武器6按鈕")]
+    public Button btnwpn6;
+
+
     public Text coin1;
     public Text coin2;
     public Text coin3;
@@ -35,6 +63,7 @@ public class MenuManager : MonoBehaviour
     public Text jewel3;
     public Text jewel4;
 
+    private Player player;
 
 
     int a = 1;
@@ -45,7 +74,7 @@ public class MenuManager : MonoBehaviour
 
     private void Start()
     {
-  
+        player = FindObjectOfType<Player>();
         data.hp = data.hpMax;
         Updatedata();
         Allowbtn();
@@ -251,11 +280,11 @@ public class MenuManager : MonoBehaviour
     /// </summary>
     private void Allowbtn()
     {
-        if ( LevelManager.bl_6== true)
+        if (LevelManager.bl_6 == true)
         {
             btn6.interactable = true;
         }
-        if ( LevelManager.bl_11== true)
+        if (LevelManager.bl_11 == true)
         {
             btn11.interactable = true;
         }
@@ -264,4 +293,252 @@ public class MenuManager : MonoBehaviour
             btn16.interactable = true;
         }
     }
+
+    /// <summary>
+    /// 開啟武器1畫面
+    /// </summary>
+    public void ShowWeapon1()
+    {
+        weapon1.SetActive(true);
+    }
+
+    /// <summary>
+    /// 開啟武器2畫面
+    /// </summary>
+    public void ShowWeapon2()
+    {
+        weapon2.SetActive(true);
+    }
+
+    /// <summary>
+    /// 開啟武器3畫面
+    /// </summary>
+    public void ShowWeapon3()
+    {
+        weapon3.SetActive(true);
+    }
+
+    /// <summary>
+    /// 開啟武器4畫面
+    /// </summary>
+    public void ShowWeapon4()
+    {
+        weapon4.SetActive(true);
+    }
+
+    /// <summary>
+    /// 開啟武器5畫面
+    /// </summary>
+    public void ShowWeapon5()
+    {
+        weapon5.SetActive(true);
+    }
+
+    /// <summary>
+    /// 開啟武器6畫面
+    /// </summary>
+    public void ShowWeapon6()
+    {
+        weapon6.SetActive(true);
+    }
+
+    /// <summary>
+    /// 關閉武器1畫面
+    /// </summary>
+    public void NoShowWeapon1()
+    {
+        weapon1.SetActive(false);
+    }
+
+    /// <summary>
+    /// 關閉武器2畫面
+    /// </summary>
+    public void NoShowWeapon2()
+    {
+        weapon2.SetActive(false);
+    }
+
+    /// <summary>
+    /// 關閉武器3畫面
+    /// </summary>
+    public void NoShowWeapon3()
+    {
+        weapon3.SetActive(false);
+    }
+
+    /// <summary>
+    /// 關閉武器4畫面
+    /// </summary>
+    public void NoShowWeapon4()
+    {
+        weapon4.SetActive(false);
+    }
+
+    /// <summary>
+    /// 關閉武器5畫面
+    /// </summary>
+    public void NoShowWeapon5()
+    {
+        weapon5.SetActive(false);
+    }
+
+    /// <summary>
+    /// 關閉武器6畫面
+    /// </summary>
+    public void NoShowWeapon6()
+    {
+        weapon6.SetActive(false);
+    }
+
+    /// <summary>
+    /// 買武器2
+    /// </summary>
+    public void BuyWeapon2()
+    {
+        if (data.PlayerCoin > 500)
+        {
+            data.PlayerCoin -= 500;
+            btnwpn2.interactable = true;
+        }
+        else
+        {
+            NoMoney();
+        }
+    }
+
+    /// <summary>
+    /// 買武器3
+    /// </summary>
+    public void BuyWeapon3()
+    {
+        if (data.PlayerCoin > 1000)
+        {
+            data.PlayerCoin -= 1000;
+            btnwpn3.interactable = true;
+        }
+        else
+        {
+            NoMoney();
+        }
+    }
+
+    /// <summary>
+    /// 買武器4
+    /// </summary>
+    public void BuyWeapon4()
+    {
+        if (data.PlayerCoin > 1500)
+        {
+            data.PlayerCoin -= 1500;
+            btnwpn4.interactable = true;
+        }
+        else
+        {
+            NoMoney();
+        }
+    }
+
+    /// <summary>
+    /// 買武器5
+    /// </summary>
+    public void BuyWeapon5()
+    {
+        if (data.PlayerCoin > 3000)
+        {
+            data.PlayerCoin -= 3000;
+            btnwpn5.interactable = true;
+        }
+        else
+        {
+            NoMoney();
+        }
+    }
+
+    /// <summary>
+    /// 買武器6
+    /// </summary>
+    public void BuyWeapon6()
+    {
+        if (data.PlayerCoin > 5000)
+        {
+            data.PlayerCoin -= 5000;
+            btnwpn6.interactable = true;
+        }
+        else
+        {
+            NoMoney();
+        }
+    }
+
+    /// <summary>
+    /// 沒錢買武器
+    /// </summary>
+    private void NoMoney()
+    {
+        Nomoney.SetActive(true);
+    }
+
+    /// <summary>
+    /// 關閉沒錢買武器畫面
+    /// </summary>
+    public void NoShowNoMoney()
+    {
+        Nomoney.SetActive(false);
+        NoShowWeapon1();
+        NoShowWeapon2();
+        NoShowWeapon3();
+        NoShowWeapon4();
+        NoShowWeapon5();
+        NoShowWeapon6();
+    }
+
+    /// <summary>
+    /// 使用武器1
+    /// </summary>
+    public void UseWeapon1()
+    {
+        GameObject weapon1 = Instantiate(Resources.Load("武器1", typeof(GameObject))) as GameObject;
+    }
+
+    /// <summary>
+    /// 使用武器2
+    /// </summary>
+    public void UseWeapon2()
+    {
+        GameObject weapon1 = Instantiate(Resources.Load("武器2", typeof(GameObject))) as GameObject;
+    }
+
+    /// <summary>
+    /// 使用武器3
+    /// </summary>
+    public void UseWeapon3()
+    {
+        GameObject weapon1 = Instantiate(Resources.Load("武器3", typeof(GameObject))) as GameObject;
+    }
+
+    /// <summary>
+    /// 使用武器4
+    /// </summary>
+    public void UseWeapon4()
+    {
+        GameObject weapon1 = Instantiate(Resources.Load("武器4", typeof(GameObject))) as GameObject;
+    }
+
+    /// <summary>
+    /// 使用武器5
+    /// </summary>
+    public void UseWeapon5()
+    {
+        GameObject weapon1 = Instantiate(Resources.Load("武器5", typeof(GameObject))) as GameObject;
+    }
+
+    /// <summary>
+    /// 使用武器6
+    /// </summary>
+    public void UseWeapon6()
+    {
+        GameObject weapon1 = Instantiate(Resources.Load("武器6", typeof(GameObject))) as GameObject;
+    }
+
+
 }
